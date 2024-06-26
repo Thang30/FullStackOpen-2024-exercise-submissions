78d8756bfdc86db953cda575a251c4ee5800e558
thang.ngo <thang.ngo@accenture.com>
2024-06-26 19:38:34 +0300

    Exercise 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,38 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import loginService from './services/login';
 import blogService from './services/blogs';
+import Blog from './components/Blog'; 
 
 const App = () => {
-  const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    blogService.getAll().then(initialBlogs => setBlogs(initialBlogs));
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
   }, []);
+
+  useEffect(() => {
+    if (user) {
+      blogService.getAll().then(blogs =>
+        setBlogs(blogs)
+      );
+    }
+  }, [user]);
 
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      const user = await loginService.login({ username, password });
+      const user = await loginService.login({
+        username, password,
+      });
+
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      );
+
+      blogService.setToken(user.token);
       setUser(user);
       setUsername('');
       setPassword('');
     } catch (exception) {
-      setErrorMessage('Wrong credentials');
-      setTimeout(() => {
-        setErrorMessage(null);
-      }, 5000);
+      console.error('Wrong credentials');
     }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    blogService.setToken(null);
+    window.localStorage.removeItem('loggedBlogAppUser');
   };
 
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
-        {errorMessage && <div className="error">{errorMessage}</div>}
         <form onSubmit={handleLogin}>
           <div>
             username
@@ -61,12 +84,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
-      {blogs.map(blog => (
-        <div key={blog.id}>
-          {blog.title} {blog.author}
-        </div>
-      ))}
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} /> // Use the Blog component
+      )}
     </div>
   );
 };
