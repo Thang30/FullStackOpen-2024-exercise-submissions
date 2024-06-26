7d69f11e8ffbd5a647ed0ac8b949dc3f2250c7b4
thang.ngo <thang.ngo@accenture.com>
2024-06-26 20:08:35 +0300

    exercise 5.5

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,22 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import loginService from './services/login';
-import blogService from './services/blogs';
+import { useState, useEffect, useRef } from 'react';
 import Blog from './components/Blog';
-import Notification from './components/Notification';
+import BlogForm from './components/BlogForm';
+import Togglable from './components/Togglable';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [newTitle, setNewTitle] = useState('');
-  const [newAuthor, setNewAuthor] = useState('');
-  const [newUrl, setNewUrl] = useState('');
+  const [user, setUser] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [user, setUser] = useState(null);
-  const [message, setMessage] = useState(null);
-  const [messageType, setMessageType] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const blogFormRef = useRef();
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser');
+    blogService.getAll().then(initialBlogs => {
+      setBlogs(initialBlogs);
+    });
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser');
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON);
       setUser(user);
@@ -24,86 +28,44 @@ const App = () => {
     }
   }, []);
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    );
-  }, []);
-
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      const user = await loginService.login({
-        username, password,
-      });
-
-      window.localStorage.setItem(
-        'loggedBloglistUser', JSON.stringify(user)
-      );
+      const user = await loginService.login({ username, password });
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user));
       blogService.setToken(user.token);
       setUser(user);
       setUsername('');
       setPassword('');
-      setMessageType('success');
-      setMessage('Successfully logged in');
-      setTimeout(() => {
-        setMessage(null);
-      }, 4000);
     } catch (exception) {
-      setMessageType('error');
-      setMessage('Wrong username or password');
+      setErrorMessage('Wrong username or password');
       setTimeout(() => {
-        setMessage(null);
-      }, 4000);
+        setErrorMessage(null);
+      }, 5000);
     }
   };
 
   const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogAppUser');
     setUser(null);
-    blogService.setToken(null);
-    window.localStorage.removeItem('loggedBloglistUser');
   };
 
-  const addBlog = async (event) => {
-    event.preventDefault();
-    try {
-      const blog = {
-        title: newTitle,
-        author: newAuthor,
-        url: newUrl,
-      };
-
-      const returnedBlog = await blogService.create(blog);
-      setBlogs(blogs.concat(returnedBlog));
-      setNewTitle('');
-      setNewAuthor('');
-      setNewUrl('');
-      setMessageType('success');
-      setMessage(`a new blog ${returnedBlog.title} by ${returnedBlog.author} added`);
-      setTimeout(() => {
-        setMessage(null);
-      }, 4000);
-    } catch (exception) {
-      setMessageType('error');
-      setMessage('Failed to add blog');
-      setTimeout(() => {
-        setMessage(null);
-      }, 4000);
-    }
+  const createBlog = async (blogObject) => {
+    blogFormRef.current.toggleVisibility();
+    const returnedBlog = await blogService.create(blogObject);
+    setBlogs(blogs.concat(returnedBlog));
   };
 
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
-        <Notification message={message} type={messageType} />
         <form onSubmit={handleLogin}>
           <div>
             username
             <input
               type="text"
               value={username}
-              name="Username"
               onChange={({ target }) => setUsername(target.value)}
             />
           </div>
@@ -112,7 +74,6 @@ const App = () => {
             <input
               type="password"
               value={password}
-              name="Password"
               onChange={({ target }) => setPassword(target.value)}
             />
           </div>
@@ -125,45 +86,16 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <Notification message={message} type={messageType} />
-      <div>
+      <p>
         {user.name} logged in
         <button onClick={handleLogout}>logout</button>
-      </div>
-      <h2>create new</h2>
-      <form onSubmit={addBlog}>
-        <div>
-          title:
-          <input
-            type="text"
-            value={newTitle}
-            name="Title"
-            onChange={({ target }) => setNewTitle(target.value)}
-          />
-        </div>
-        <div>
-          author:
-          <input
-            type="text"
-            value={newAuthor}
-            name="Author"
-            onChange={({ target }) => setNewAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url:
-          <input
-            type="text"
-            value={newUrl}
-            name="Url"
-            onChange={({ target }) => setNewUrl(target.value)}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
-      {blogs.map(blog =>
+      </p>
+      <Togglable buttonLabel="new note" ref={blogFormRef}>
+        <BlogForm createBlog={createBlog} />
+      </Togglable>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
   );
 };
