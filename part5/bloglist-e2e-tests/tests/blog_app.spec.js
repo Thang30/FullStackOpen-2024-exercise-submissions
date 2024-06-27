20595f31ebaa310d7a8273fc19240921321446e2
thang.ngo <thang.ngo@accenture.com>
2024-06-27 11:24:03 +0300

    exercise 5.22

--- part5/bloglist-e2e-tests/tests/blog_app.spec.js
+++ part5/bloglist-e2e-tests/tests/blog_app.spec.js
@@ -5,14 +5,23 @@ describe('Blog app', () => {
     const resetResponse = await request.post('/api/testing/reset');
     console.log(`Database reset response status: ${resetResponse.status()}`);
 
-    const userResponse = await request.post('/api/users', {
+    const userResponse1 = await request.post('/api/users', {
       data: {
-        name: 'Test User',
-        username: 'testuser',
-        password: 'testpassword'
+        name: 'Test User 1',
+        username: 'testuser1',
+        password: 'testpassword1'
       }
     });
-    console.log(`User creation response status: ${userResponse.status()}`);
+    console.log(`User creation response status (Test User 1): ${userResponse1.status()}`);
+
+    const userResponse2 = await request.post('/api/users', {
+      data: {
+        name: 'Test User 2',
+        username: 'testuser2',
+        password: 'testpassword2'
+      }
+    });
+    console.log(`User creation response status (Test User 2): ${userResponse2.status()}`);
 
     await page.goto('http://localhost:5173');
   });
@@ -28,8 +37,8 @@ describe('Blog app', () => {
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
       await page.click('text=log in');
-      await page.fill('input[name="Username"]', 'testuser');
-      await page.fill('input[name="Password"]', 'testpassword');
+      await page.fill('input[name="Username"]', 'testuser1');
+      await page.fill('input[name="Password"]', 'testpassword1');
       await page.click('text=login');
       const logoutButton = page.getByText('logout');
       await expect(logoutButton).toBeVisible();
@@ -37,15 +46,14 @@ describe('Blog app', () => {
 
     test('fails with wrong credentials', async ({ page }) => {
       await page.click('text=log in');
-      await page.fill('input[name="Username"]', 'testuser');
+      await page.fill('input[name="Username"]', 'testuser1');
       await page.fill('input[name="Password"]', 'wrongpassword');
       await page.click('text=login');
       
-      // Take a screenshot before checking the error message
       await page.screenshot({ path: 'screenshots/before_checking_error_message.png' });
 
       const errorMessage = page.getByText('Wrong credentials');
-      await expect(errorMessage).toBeVisible({ timeout: 10000 }); // Extend the timeout to 10 seconds
+      await expect(errorMessage).toBeVisible({ timeout: 10000 });
     });
   });
 
@@ -53,61 +61,37 @@ describe('Blog app', () => {
     beforeEach(async ({ page }) => {
       await page.goto('http://localhost:5173');
       await page.click('text=log in');
-      await page.fill('input[name="Username"]', 'testuser');
-      await page.fill('input[name="Password"]', 'testpassword');
+      await page.fill('input[name="Username"]', 'testuser1');
+      await page.fill('input[name="Password"]', 'testpassword1');
       await page.click('text=login');
       const logoutButton = page.getByText('logout');
       await expect(logoutButton).toBeVisible();
-        console.log("Successfully logged in!");
+      console.log('Successfully logged in!');
     });
 
     test('a new blog can be created', async ({ page }) => {
-      // Click the "create new blog" button to reveal the blog form
       await page.click('text=create new blog');
-
-      // Fill in the blog details
       await page.fill('input[placeholder="title"]', 'New Blog Title');
       await page.fill('input[placeholder="author"]', 'Blog Author');
       await page.fill('input[placeholder="url"]', 'http://newblogurl.com');
-
-      // Click the "create" button to submit the blog
-        //   await page.click('button:text("create")');
       await page.click('form >> text=create');
-
-
-      // Verify the new blog is visible
       const newBlogTitle = page.locator('.blog-title:has-text("New Blog Title")');
       await expect(newBlogTitle).toBeVisible();
     });
-      
-    test('a blog can be liked', async ({ page }) => {
-      // Click the "create new blog" button to reveal the blog form
-      await page.click('text=create new blog');
 
-      // Fill in the blog details
+    test('a blog can be liked', async ({ page }) => {
+      await page.click('text=create new blog');
       await page.fill('input[placeholder="title"]', 'New Blog Title');
       await page.fill('input[placeholder="author"]', 'Blog Author');
       await page.fill('input[placeholder="url"]', 'http://newblogurl.com');
-
-      // Click the "create" button to submit the blog
       await page.click('form >> text=create');
-
-      // Verify the new blog is visible in the list of blogs
       const newBlogTitle = page.locator('.blog-title:has-text("New Blog Title")');
       await expect(newBlogTitle).toBeVisible();
-
-      // Click the "view" button to reveal blog details
       await page.click('.blog >> text=view');
-
-      // Get the current number of likes
       const likeCountElement = page.locator('.blog-likes');
       const initialLikeCountText = await likeCountElement.textContent();
       const initialLikeCount = parseInt(initialLikeCountText.match(/\d+/)[0]);
-      console.log("The initial like count is:", initialLikeCount);
-      // Click the "like" button
       await page.click('.blog-likes >> text=like');
-
-      // Wait for the like count to increase
       await expect(likeCountElement).toHaveText(`likes ${initialLikeCount + 1} like`);
     });
 
@@ -121,21 +105,39 @@ describe('Blog app', () => {
       await expect(newBlogTitle).toBeVisible();
       await page.click('.blog >> text=view');
       
-      // Intercept the confirmation dialog and accept it
       page.on('dialog', async dialog => {
         await dialog.accept();
       });
 
-      // Ensure the remove button is visible before clicking
       const removeButton = page.locator('.blog >> text=remove');
       await expect(removeButton).toBeVisible();
-
-      // Click the "remove" button to delete the blog
       await removeButton.click();
-
-      // Verify the blog is no longer visible
       await expect(newBlogTitle).not.toBeVisible();
     });
 
+    test('only the user who added the blog sees the delete button', async ({ page }) => {
+      await page.click('text=create new blog');
+      await page.fill('input[placeholder="title"]', 'User 1 Blog Title');
+      await page.fill('input[placeholder="author"]', 'User 1');
+      await page.fill('input[placeholder="url"]', 'http://user1blogurl.com');
+      await page.click('form >> text=create');
+      const user1BlogTitle = page.locator('.blog-title:has-text("User 1 Blog Title")');
+      await expect(user1BlogTitle).toBeVisible();
+
+      await page.click('text=logout');
+      const loginButton = page.getByRole('button', { name: 'log in' });
+      await expect(loginButton).toBeVisible();
+
+      await page.click('text=log in');
+      await page.fill('input[name="Username"]', 'testuser2');
+      await page.fill('input[name="Password"]', 'testpassword2');
+      await page.click('text=login');
+      const logoutButton = page.getByText('logout');
+      await expect(logoutButton).toBeVisible();
+
+      await page.click('.blog >> text=view');
+      const removeButton = page.locator('.blog >> text=remove');
+      await expect(removeButton).not.toBeVisible();
+    });
   });
 });
